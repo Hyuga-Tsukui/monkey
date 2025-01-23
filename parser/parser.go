22371c9c06e425bc88f7a9d3c318dec3bf656c71
Hyuga Tsukui <gakikiki407@gmail.com>
2025-01-23 22:06:35 +0900

    add: parser errors

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/Hyuga-Tsukui/monkey/ast"
 	"github.com/Hyuga-Tsukui/monkey/lexer"
 	"github.com/Hyuga-Tsukui/monkey/token"
@@ -11,10 +13,21 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	p.nextToken()
 	p.nextToken()
@@ -82,6 +95,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/Hyuga-Tsukui/monkey/ast"
@@ -8,36 +9,66 @@ import (
 )
 
 func TestLetStatements(t *testing.T) {
-	input := `
-let x = 5;
-let y = 10;
-let foobar = 838383;
-	`
-
-	l := lexer.New(input)
-	p := New(l)
-
-	program := p.ParseProgram()
-	if program == nil {
-		t.Fatalf("ParseProgram() returned nil")
-	}
-	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements dose not contain 3 statements. got=%d dump=%#v", len(program.Statements), program)
-	}
-
 	tests := []struct {
-		expectedIdentifier string
+		name               string
+		input              string
+		statementCount     int
+		expectedIdentifier []string
+		hasParseError      bool
+		expectedErrors     []string
 	}{
-		{"x"},
-		{"y"},
-		{"foobar"},
+		{
+			name: "let statement",
+			input: `
+			let x = 5;
+			let y = 10;
+			let foobar = 838383;
+			`,
+			statementCount:     3,
+			expectedIdentifier: []string{"x", "y", "foobar"},
+		},
+		{
+			name: "let statement with parse error",
+			input: `
+			let x = 5;
+			let = 10;
+			let foobar = 838383;
+			`,
+			hasParseError: true,
+			expectedErrors: []string{
+				"expected next token to be IDENT, got = instead",
+			},
+		},
 	}
 
-	for i, tt := range tests {
-		stmt := program.Statements[i]
-		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
-			return
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			l := lexer.New(tt.input)
+			p := New(l)
+
+			program := p.ParseProgram()
+			if tt.hasParseError {
+				if !reflect.DeepEqual(p.errors, tt.expectedErrors) {
+					t.Errorf("p.errors = %v, want %v", p.errors, tt.expectedErrors)
+					t.Fatalf("ParseProgram() has parse error, but not expected error")
+				}
+				return
+			}
+			if program == nil {
+				t.Fatalf("ParseProgram() returned nil")
+			}
+			if len(program.Statements) != tt.statementCount {
+				t.Fatalf("program.Statements dose not contain %d statements. got=%d dump=%#v", tt.statementCount, len(program.Statements), program)
+			}
+
+			for i := 0; i < tt.statementCount; i++ {
+				stmt := program.Statements[i]
+				if !testLetStatement(t, stmt, tt.expectedIdentifier[i]) {
+					return
+				}
+			}
+		})
 	}
 }
 
@@ -60,4 +91,18 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser, expectedErrors []string) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	if !reflect.DeepEqual(errors, expectedErrors) {
+		t.Fail()
+	}
 }
